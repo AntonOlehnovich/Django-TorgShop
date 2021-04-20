82b39959190fda63662a6bd2c15ec74d13def158
AntonOlehnovich <generato9999@mail.ru>
2021-04-20 20:02:01 +0300

    Setting, About US, Contact US

--- TorgShop/urls.py
+++ TorgShop/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+
+import home
+from home import views
+
 urlpatterns = [
     path('', include('home.urls')),
     path('home/', include('home.urls')),
+    path('about/', views.aboutus, name='aboutus'),
+    path('contact/', views.contactus, name='contactus'),
     path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
--- home/views.py
+++ home/views.py
@@ -2,8 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import Setting
+
+
 def index(request):
-    university = "Baranovichi University"
-    dept = "Information Systems"
-    context = {'university':university, 'department':dept}
+    setting = Setting.objects.get(pk=1)
+    page = "home"
+    context = {'setting':setting, 'page': page}
     return render(request, 'index.html', context)
+
+
+def aboutus(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'about.html', context)
+
+def contactus(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'contactus.html', context)
