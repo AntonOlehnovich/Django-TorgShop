e62df984ae9f57a9b16ddfbd6149b7ba092dbf7f
AntonOlehnovich <generato9999@mail.ru>
2021-04-22 20:03:00 +0300

    Autocomplete in Search by Using AJAX JQuery

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -76,3 +78,18 @@ def search(request):
             return render(request, 'search_products.html', context)
 
     return HttpResponseRedirect('/')
+
+def search_auto(request):
+  if request.is_ajax():
+    q = request.GET.get('term', '')
+    products = Product.objects.filter(title__icontains=q)
+    results = []
+    for rs in products:
+      product_json = {}
+      product_json = rs.title
+      results.append(product_json)
+    data = json.dumps(results)
+  else:
+    data = 'fail'
+  mimetype = 'application/json'
+  return HttpResponse(data, mimetype)
