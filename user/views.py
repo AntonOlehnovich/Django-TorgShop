2316394df4315a4994c4513131676b6b8b012171
AntonOlehnovich <generato9999@mail.ru>
2021-04-26 12:33:04 +0300

    Change User Password

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -91,3 +92,21 @@ def user_update(request):
             'profile_form': profile_form
         }
         return render(request, 'user_update.html', context)
+
+
+@login_required(login_url='/login') # Check login
+def user_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Ваш пароль успешно изменён!')
+            return HttpResponseRedirect('/user')
+        else:
+            messages.error(request, 'Пожалуйста, исправьте ошибку.<br>' + str(form.errors))
+            return HttpResponseRedirect('/user/password')
+    else:
+        category = Category.objects.all()
+        form = PasswordChangeForm(request.user)
+        return render(request, 'user_password.html', {'form': form,  'category': category})
