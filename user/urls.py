c12018a51fbb12d3d6033df60aeaf680a66fe001
AntonOlehnovich <generato9999@mail.ru>
2021-04-26 18:16:30 +0300

    User Comments List and Delete

--- user/urls.py
+++ user/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('orders_product/', views.user_orders_product, name='user_orders_product'),
     path('orderdetail/<int:id>', views.user_orderdetail, name='user_orderdetail'),
     path('order_product_detail/<int:id>/<int:oid>', views.user_order_product_detail, name='user_order_product_detail'),
+    path('comments/', views.user_comments, name='user_comments'),
+    path('deletecomment/<int:id>', views.user_deletecomment, name='user_deletecomment'),
 ]
